262ccabd140304338f2f2ae648badcf56c513b40
Niko Heikkilä <niko@heikkila.io>
2024-05-15 16:02:05 +0300

    fix: remove redundant timer checks

--- src/timer.service.ts
+++ src/timer.service.ts
@@ -3,29 +3,17 @@ import { Timer } from './interfaces';
 
 @Injectable()
 export class TimerService implements Timer {
-  private isRunning: boolean;
   private startTime: number;
 
   constructor() {
-    this.isRunning = false;
     this.startTime = 0;
   }
 
   public start(): void {
-    if (this.isRunning) {
-      throw new Error('Timer is already running');
-    }
-
     this.startTime = Date.now();
-    this.isRunning = true;
   }
 
   stop(): number {
-    if (!this.isRunning) {
-      throw new Error('Timer is not running');
-    }
-
-    this.isRunning = false;
     return Date.now() - this.startTime;
   }
 }
