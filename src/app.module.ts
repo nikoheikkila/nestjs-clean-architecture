adf4f8d3afcd01d6855ff5ab9b51b12eb1fbbc02
Niko Heikkilä <niko@heikkila.io>
2024-05-15 11:00:29 +0300

    feat: setup ChatController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatControllerController } from './chat-controller/chat-controller.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ChatControllerController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/chat-controller/chat-controller.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('chat-controller')
+export class ChatControllerController {}
