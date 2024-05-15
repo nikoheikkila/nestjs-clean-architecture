77a5a1bb1965e4e3d21a030702009d676faa878e
Niko Heikkilä <niko@heikkila.io>
2024-05-15 16:10:01 +0300

    refactor: chat.service.ts

--- src/chat.service.ts
+++ src/chat.service.ts
@@ -2,27 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { ChatOptions, ChatService } from './interfaces';
 import OpenAI from 'openai';
 
+enum Model {
+  OMNI = 'gpt-4o',
+}
+
+enum Role {
+  SYSTEM = 'system',
+  USER = 'user',
+}
+
 @Injectable()
 export class OpenAIChatService implements ChatService {
   public async generateAnswer(
     prompt: string,
     options: ChatOptions,
   ): Promise<string> {
-    const api = new OpenAI({
-      apiKey: options.token,
-    });
+    const api = this.createClient(options.token);
 
+    return this.generate(api, prompt, options);
+  }
+
+  private async generate(api: OpenAI, prompt: string, options: ChatOptions) {
     const completion = await api.chat.completions.create({
-      model: "gpt-4o",
+      model: Model.OMNI,
       temperature: options.temperature,
       messages: [
         {
-          role: 'user',
+          role: Role.USER,
           content: prompt,
         },
       ],
     });
 
     return completion.choices[0].message.content;
+  }
+
+  private createClient(token: string) {
+    return new OpenAI({
+      apiKey: token,
+    });
   }
 }
